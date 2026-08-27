d10efe464918c53c1aab6997c720d5edd1ad0bec
baptisteclaustre <baptiste.claustre@outlook.fr>
2026-08-27 13:17:07 +0200

    Update .eleventy.js
    sitemap export

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images"); // ajouter le doss img
   eleventyConfig.addPassthroughCopy("src/pdf"); // idem pdf
+   eleventyConfig.addPassthroughCopy("src/sitemap.xml"); 
+};
 
   return {
     dir: {
